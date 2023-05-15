210da3ae96b836894d482a79fc8dc0ac40da950b
Y.J <yanjun_hao@outlook.com>
2023-05-15 16:32:37 +0800

    V1.0

--- SpectralClustering.py
+++ SpectralClustering.py
@@ -49,11 +49,14 @@ def plot_silhouette_of_various_clusters(eps_li: list, min_samples: int=MIN_SAMPL
 
 if __name__ == "__main__":
     origin_df = pd.read_excel("./datasets/副本shujulianxi.xlsx", sheet_name=0)
+    origin_df['拍摄时间'] = pd.to_datetime(origin_df['拍摄时间']).dt.strftime("%Y-%m-%d")
+    origin_df.drop_duplicates(subset=['用户ID', '拍摄时间'], keep="first", inplace=True)
     print("Data Load Success!!!")
-    X = origin_df[['经度', '纬度']].astype('float16')  # .iloc[:5000]
-    # silhouette_score_df = plot_silhouette_of_various_clusters(eps_li=[0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6])
+    X = origin_df[['经度', '纬度']].astype('float16').iloc[:100000]
+    silhouette_score_df = plot_silhouette_of_various_clusters(eps_li=[0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6])
+    silhouette_score_df.to_csv("./assets/silhouette_score_df.csv", index=False, encoding="utf-8-sig")
     # 数据量太大，只能单个跑
-    silhouette_score_df = plot_silhouette_of_various_clusters(eps_li=[0.15])
-    part_df = pd.read_csv(r"./assets/silhouette_score_df.csv")
-    complete_df = pd.concat([part_df, silhouette_score_df], axis=0, ignore_index=True)
-    complete_df.to_csv("./assets/silhouette_score_df.csv", index=False, encoding="utf-8-sig")
+    # silhouette_score_df = plot_silhouette_of_various_clusters(eps_li=[0.15])
+    # part_df = pd.read_csv(r"./assets/silhouette_score_df.csv")
+    # complete_df = pd.concat([part_df, silhouette_score_df], axis=0, ignore_index=True)
+    # complete_df.to_csv("./assets/silhouette_score_df.csv", index=False, encoding="utf-8-sig")
